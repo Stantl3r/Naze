911be5134294ab7dd8597d5315ad87eb16183f47
Stanley Tran <Stanley@eduroam-169-233-167-90.ucsc.edu>
2019-01-19 19:22:05 -0800

    Combined locations with the map

--- maps.js
+++ maps.js
@@ -14,7 +14,9 @@ function initMap() {
             });
             marker.setMap(map)
             directionsDisplay.setMap(map);
+        	getShelters(getCity(position.coords.latitude, position.coords.longitude));
             calculateAndDisplayRoute(directionsService, directionsDisplay, position, '4115 Voltaire Street');
+            
 		})
 	}
 
@@ -32,5 +34,43 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay, position
 	});
 }
 
+function getCity(lat, lng) {
+	city = '';
+	var latlng = new google.maps.LatLng(lat, lng);
+	geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			if (results[1]) {
+				city = results[0].formatted_address.split(',')[2];
+        	} 
+      	}
+    });
+    return city;
+}
+
+function getShelters(city) {
+	city = city.split(' ');
+	url = 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/National_Shelter_System_Facilities/FeatureServer/0/query?where=CITY%20like%20\'%25';
+	for (i = 0; i < city.length; i++) {
+		if (i == city.length - 1) {
+			url += city[i];
+		} else {
+			url += (city[i] + '%20');
+		}
+	}
+	url += '%25\'&outFields=*&outSR=4326&f=json';
+	var xhr = new XMLHttpRequest(); 
+	xhr.open("GET", url, false);
+	xhr.send(null);
+
+	shelter_lat = [];
+	shelter_lng = [];
+        
+	var shelters_json = JSON.parse(xhr.responseText);
+	for (i = 0; i < shelters_json.features.length; i++) {
+		shelter_lat.push(shelters_json.features[i].attributes.LATITUDE);
+		shelter_lng.push(shelters_json.features[i].attributes.LONGITUDE);
+	}
+}
 	
 }
