825bbc9112e6fa17796a3dc74d6bee7448ebdd21
Michael Wang <32807138+theMichaelWang@users.noreply.github.com>
2019-01-19 23:49:13 -0800

    Updated maps.js
    Finds closest emergency shelter from current location

--- maps.js
+++ maps.js
@@ -3,27 +3,41 @@ function initMap() {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			var map = new google.maps.Map(document.getElementById('map'), {zoom: 14, center: {lat: position.coords.latitude, lng: position.coords.longitude}})
 			var pos = {lat: position.coords.latitude, lng: position.coords.longitude}
-			var currentPositionLabel = 'YOU'
 			var directionsDisplay = new google.maps.DirectionsRenderer;
 			var directionsService = new google.maps.DirectionsService;
 			var markers = [];
-			//for (var i=0; i<)
 			marker = new google.maps.Marker({
 				position: pos,
 				animation: google.maps.Animation.DROP,
 				draggable: true,
-				label: currentPositionLabel
             });
             marker.setMap(map);
-            marker1 = new google.maps.Marker({
-            	position: {lat: 37.77, lng: -122.447}
-            })
-            marker1.setMap(map);
             directionsDisplay.setMap(map);
-			getShelters(getCity(position.coords.latitude, position.coords.longitude));            
-			calculateAndDisplayRoute(directionsService, directionsDisplay, position, marker1.getPosition());
+			var loc = getShelters(getCity(position.coords.latitude, position.coords.longitude));
+			for(var i = 0; i<loc.shelter_lat.length; i++){
+				markers[i] = new google.maps.Marker({
+					position: new google.maps.LatLng(loc.shelter_lat[i],loc.shelter_lng[i]),
+					map: map
+				});
+			}
+			var distance = 100000000000;
+			var closest_index = 0;
+			for(var j = 0; j<markers.length; j++){
+				var dlon = (loc.shelter_lng[j]-position.coords.longitude)*(Math.PI/180);
+				var dlat = (loc.shelter_lat[j]-position.coords.latitude)*(Math.PI/180);
+				var a = Math.sin(dlat/2)*Math.sin(dlat/2) + Math.cos(position.coords.latitude*(Math.PI/180)) *
+				Math.cos(loc.shelter_lat[j]*(Math.PI/180))* Math.sin(dlon/2)*Math.sin(dlon/2);
+				var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+				var d = 6371* c;
+				if(d<distance){
+					distance = d;
+					closest_index = j;
+				}
+			}
+			markers[closest_index].setMap(map);       
+			calculateAndDisplayRoute(directionsService, directionsDisplay, position, markers[closest_index].getPosition());
 			document.getElementById('mode').addEventListener('change', function() {
-           		calculateAndDisplayRoute(directionsService, directionsDisplay, position, marker1.getPosition());
+           		calculateAndDisplayRoute(directionsService, directionsDisplay, position, markers[closest_index].getPosition());
         	});
 		})
 	}
@@ -42,25 +56,20 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay, position
 		}
 	});
 }
+
 function getCity(lat, lng) {
-	city = '';
-	var latlng = new google.maps.LatLng(lat, lng);
-	geocoder = new google.maps.Geocoder();
-	geocoder.geocode({'latLng': latlng}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			if (results[1]) {
-				city = results[0].formatted_address.split(',')[2];
-			} 
-	  	}
-	});
-	return city;
+	var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lng + '&key=AIzaSyCykp6C4Ivqy-lhHMB1It1UcZnHkoVMU8Y';
+	var xhr = new XMLHttpRequest(); 
+	xhr.open("GET", url, false);
+	xhr.send(null);
+	var location_json = JSON.parse(xhr.responseText);
+	return location_json.results[0].formatted_address.split(',')[2]
 }
 
 function getShelters(city) {
-	console.log(city)
 	city = city.split(' ');
 	url = 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/National_Shelter_System_Facilities/FeatureServer/0/query?where=CITY%20like%20\'%25';
-	for (i = 0; i < city.length; i++) {
+	for (i = 1; i < city.length; i++) {
 		if (i == city.length - 1) {
 			url += city[i];
 		} else {
@@ -79,7 +88,10 @@ function getShelters(city) {
 		shelter_lat.push(shelters_json.features[i].attributes.LATITUDE);
 		shelter_lng.push(shelters_json.features[i].attributes.LONGITUDE);
 	}
-	return shelter_lat
+	return{
+		shelter_lat: shelter_lat,
+		shelter_lng: shelter_lng
+	};
 }
 	
 }
